d384becc0f7db85e5e1e3e11bdca10e33496c084
Andri Burman <andri.burman@hellopay.co.id>
2017-01-20 22:20:36 +0700

    modified to run with python 3.6

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app, lm
 from flask import request, redirect, render_template, url_for, flash
-from flask.ext.login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm
 from .user import User
 
